2d4d8ba89ed3082c7eda907a54c5067f9990a3d3
yyy9942 <yyy9942@naver.com>
2020-05-10 21:52:27 +0900

    webpack study

---
+++ NumberBaseball.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <div>{{result}}</div>    
+    <form v-on:submit="onSubmitForm">
+        <input ref="ansert" maxlength="4" v-model="value"/>
+        <button>입력</button>
+        <div>시도: {{}}</div>
+    </form>
+</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                value: '',
+                result: '',
+            }
+        },
+        methods: {
+            onSubmitForm(e) {
+                e.preventDefault();
+                
+            }
+        }
+    };
+</script>
+
+<style>
+
+</style>
